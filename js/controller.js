e05ed0121ae1c6aa1d85d7b037b39908f6ad890c
tujh <tujh@okooo.net>
2016-03-29 21:53:23 +0800

    list
    list

--- js/controller.js
+++ js/controller.js
@@ -16,10 +16,22 @@
   })
 
   testController.controller("syCtrl",function($scope){
-    $scope.message = "我是首页";
+    $scope.list = [
+      {id:"baba",name:"臭臭",img:"img/baba.png"},
+      {id:"home",name:"宅着",img:"img/home.png"},
+      {id:"love",name:"爱爱",img:"img/love.png"},
+      {id:"movie",name:"看电影",img:"img/movie.png"},
+      {id:"shopping",name:"购物",img:"img/shopping.png"},
+      {id:"sport",name:"运动",img:"img/sport.png"},
+      {id:"travel",name:"旅游",img:"img/travel.png"},
+    ];
+
+    $scope.addEvent = function(id,i){
+      alert($scope.list[i]['name']);
+    }
   });
 
   testController.controller("histCtrl",function($scope){
-    $scope.message = "我是历史页面";
+    //$scope.message = "我是历史页面";
   });
 })();
