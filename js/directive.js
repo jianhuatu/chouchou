65f53b19833788a7f71d98348cb7ddb2b0c866b8
jianhuatu <tujianhua123@gmail.com>
2016-03-30 09:14:30 +0800

    版本合并
    版本合并

--- js/directive.js
+++ js/directive.js
@@ -1,6 +1,18 @@
 (function(){
   var testDirective = angular.module("testDirective",[]);
 
+  testDirective.directive("noTouchMove",function(){
+  	return {
+  		restrict : "A",
+  		link : function(scope, element, attrs){
+  			element[0].addEventListener("touchmove",function(e){
+  				e.preventDefault();
+  				return false;
+  			},false);
+  		}
+  	}
+  });
+
   testDirective.directive("domIscroll",function(jQGlobalData,mainHeight){
   	return {
   		restrict : "A",
